09b67ee566c27c05c00a0bfada5cc74401bcd3c6
marco m <inspiron0925@gmail.com>
2022-05-12 22:28:46 -0700

    make a number negative

---
+++ negative.js
@@ -0,0 +1,9 @@
+
+// The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+function makeNegative(num) {
+    // Code?
+    if(Math.sign(num)=== -1){
+  return num
+    }return num * -1
+  }
