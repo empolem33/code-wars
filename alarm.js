fcd3bff838be1b923461d90625470d902b2325e6
empolem33 <inspiron0925@yahoo.com>
2022-07-02 19:09:03 -0700

    return else

--- alarm.js
+++ alarm.js
@@ -3,5 +3,13 @@
 //and the second parameter, vacation is true whenever you are on vacation.
 
 function setAlarm(employed, vacation){
-
-}
+    if(employed && vacation || !employed && !vacation){
+        return false
+      }if(!employed && vacation){
+          return false
+        }else{
+          return true
+        }
+      }
+        
+      
