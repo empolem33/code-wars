38714c9f2df40797894306e06d76130e7d533e98
empolem33 <inspiron0925@yahoo.com>
2022-07-04 15:14:11 -0700

    modulus 3 or 5

--- katasix.js
+++ katasix.js
@@ -4,3 +4,18 @@
 // multiples of 3 or 5 below the number passed in. Additionally, 
 // if the number is negative, return 0 (for languages that do have them).
 // Note: If the number is a multiple of both 3 and 5, only count it once.
+
+function solution(number){
+    if(number < 0){
+      return 0
+    }else {
+      box=0
+      for(let i=1; i<number;i++){
+        if(i%3==0 || i%5==0){
+          box+=i
+          
+        }
+      }
+      return box
+    }
+  }
