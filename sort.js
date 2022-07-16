a8af8c7870922c50d623909a2b9d3afa4833e86e
empolem33 <inspiron0925@yahoo.com>
2022-07-16 15:06:43 -0700

    finished

--- sort.js
+++ sort.js
@@ -5,3 +5,12 @@
 // Given [34, 15, 88, 2] your solution will return 2
 // Given [34, -345, -1, 100] your solution will return -345
 // You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      args.sort(function(a, b) {
+    return a - b;
+  });
+    return args[0]
+    }
+  }
