03a0495e06230e4cf145d6eaeb02bbe9f1578794
empolem33 <inspiron0925@yahoo.com>
2022-07-20 12:58:08 -0700

    fixed minus sort

--- arrsort.js
+++ arrsort.js
@@ -14,7 +14,7 @@ function sortByLength (array) {
     // Return an array containing the same strings, ordered from shortest to longest
     array.join()
     array.sort(function(a, b) {
-    return a.length > b.length;
+    return a.length - b.length;
   })
     return array
   };
